2f3f99e7c4a4d5b2be5cf228116bb37843203b2c
Achraf Ben Soltane <achrafb.s2015@gmail.com>
2023-03-05 14:42:04 +0100

    hello world

--- backend/src/main/java/com/example/tunisair/TunisairApplication.java
+++ backend/src/main/java/com/example/tunisair/TunisairApplication.java
@@ -2,12 +2,21 @@ package com.example.tunisair;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @SpringBootApplication
+@RestController
 public class TunisairApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TunisairApplication.class, args);
 	}
 
+	@GetMapping
+	public String hello() {
+		return "Hello World";
+	}
 }
