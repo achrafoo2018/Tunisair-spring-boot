3c80062f30ce898f78854b84543c8e4d83b10aeb
Achraf Ben Soltane <achrafb.s2015@gmail.com>
2023-03-05 17:30:32 +0100

    flight model

--- backend/src/main/java/com/example/tunisair/models/Flight.java
+++ backend/src/main/java/com/example/tunisair/models/Flight.java
@@ -1,5 +1,140 @@
 package com.example.tunisair.models;
+import java.time.LocalDateTime;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "flights")
 public class Flight {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name = "flight_number")
+    private String flightNumber;
+    
+    @Column(name = "airline")
+    private String airline;
+    
+    @Column(name = "departure_airport")
+    private String departureAirport;
+    
+    @Column(name = "arrival_airport")
+    private String arrivalAirport;
+    
+    @Column(name = "departure_time")
+    private LocalDateTime departureTime;
+    
+    @Column(name = "arrival_time")
+    private LocalDateTime arrivalTime;
+    
+    @Column(name = "seats_available")
+    private int seatsAvailable;
+    
+    @Column(name = "price")
+    private double price;
+    // constructors, getters and setters
+    
+    public Flight() {}
+
+    public Flight(String flightNumber, String airline, String departureAirport, String arrivalAirport,
+            LocalDateTime departureTime, LocalDateTime arrivalTime, int seatsAvailable, double price) {
+        this.flightNumber = flightNumber;
+        this.airline = airline;
+        this.departureAirport = departureAirport;
+        this.arrivalAirport = arrivalAirport;
+        this.departureTime = departureTime;
+        this.arrivalTime = arrivalTime;
+        this.seatsAvailable = seatsAvailable;
+        this.price = price;
+    }
+
+    // getters and setters for each attribute
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public void setFlightNumber(String flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public String getAirline() {
+        return airline;
+    }
+
+    public void setAirline(String airline) {
+        this.airline = airline;
+    }
+
+    public String getDepartureAirport() {
+        return departureAirport;
+    }
+
+    public void setDepartureAirport(String departureAirport) {
+        this.departureAirport = departureAirport;
+    }
+
+    public String getArrivalAirport() {
+        return arrivalAirport;
+    }
+
+    public void setArrivalAirport(String arrivalAirport) {
+        this.arrivalAirport = arrivalAirport;
+    }
+
+    public LocalDateTime getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(LocalDateTime departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public LocalDateTime getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(LocalDateTime arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public int getSeatsAvailable() {
+        return seatsAvailable;
+    }
+
+    public void setSeatsAvailable(int seatsAvailable) {
+        this.seatsAvailable = seatsAvailable;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    
+    //toString
+    @Override
+    public String toString() {
+        return "Flight [id=" + id + ", flightNumber=" + flightNumber + ", airline=" + airline + ", departureAirport="
+                + departureAirport + ", arrivalAirport=" + arrivalAirport + ", departureTime=" + departureTime
+                + ", arrivalTime=" + arrivalTime + ", seatsAvailable=" + seatsAvailable + ", price=" + price + "]";
+    }
 }
+
